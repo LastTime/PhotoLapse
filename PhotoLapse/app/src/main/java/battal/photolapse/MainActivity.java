c1bd5362e90013ca7caf19cca8ee35c8d6b0d270
LastTime <unalbattal@gmail.com>
2016-11-04 09:52:59 +0100

    Project Startup
    This is the time 0 for my project. There is only a Hello World
    application with an empty activity.

---
+++ PhotoLapse/app/src/main/java/battal/photolapse/MainActivity.java
@@ -0,0 +1,13 @@
+package battal.photolapse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
